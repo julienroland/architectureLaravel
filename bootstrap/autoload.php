7f6fb6dd69fd349373341b7506beb476514a5992
julienroland <roland.julien.perso@gmail.com>
2014-11-15 15:12:29 +0100

    init

---
+++ bootstrap/autoload.php
@@ -0,0 +1,75 @@
+<?php
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Include The Compiled Class File
+|--------------------------------------------------------------------------
+|
+| To dramatically increase your application's performance, you may use a
+| compiled class file which contains all of the classes commonly used
+| by a request. The Artisan "optimize" is used to create this file.
+|
+*/
+
+$compiledPath = __DIR__.'/../storage/framework/compiled.php';
+
+if (file_exists($compiledPath))
+{
+	require $compiledPath;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Setup Patchwork UTF-8 Handling
+|--------------------------------------------------------------------------
+|
+| The Patchwork library provides solid handling of UTF-8 strings as well
+| as provides replacements for all mb_* and iconv type functions that
+| are not available by default in PHP. We'll setup this stuff here.
+|
+*/
+Patchwork\Utf8\Bootup::initMbstring();
+
+/*
+|--------------------------------------------------------------------------
+| Register The Workbench Loaders
+|--------------------------------------------------------------------------
+|
+| The Laravel workbench provides a convenient place to develop packages
+| when working locally. However we will need to load in the Composer
+| auto-load files for the packages so that these can be used here.
+|
+*/
+
+if (is_dir($workbench = __DIR__.'/../workbench'))
+{
+    Illuminate\Workbench\Starter::start($workbench);
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The October Auto Loader
+|--------------------------------------------------------------------------
+| This should come before the Laravel loader because it is more likely
+| to find a partner fo' life.
+|
+*/
+
+Cms\Support\ClassLoader::register();
+Cms\Support\ClassLoader::addDirectories(array(__DIR__.'/../modules'));
