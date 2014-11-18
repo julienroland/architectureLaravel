bee043e9abe8c7be65fa7f3bde64701498122246
julienroland <roland.julien.perso@gmail.com>
2014-11-18 20:56:24 +0100

    init migrator to make custom migration command

--- workbench/dogstudio/cms/src/Cms/Console/Module/Commands/MigrateCommand.php
+++ workbench/dogstudio/cms/src/Cms/Console/Module/Commands/MigrateCommand.php
@@ -2,6 +2,7 @@
 
 use Cms\Modules\Traits\ModulesTrait;
 use Illuminate\Console\Command;
+use Illuminate\Database\Migrations\Migrator;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Illuminate\Support\Str;
@@ -9,6 +10,16 @@ use Illuminate\Support\Str;
 class MigrateCommand extends Command
 {
     use ModulesTrait;
+
+    /**
+     * @var
+     */
+    private $migrator;
+
+    public function __construct(Migrator $migrator)
+    {
+        $this->migrator = $migrator;
+    }
 
     protected $name = 'module:migrate';
 
@@ -20,7 +31,7 @@ class MigrateCommand extends Command
         $module = Str::studly($this->argument('module'));
         if ($module) {
             $this->comment("Migrating module: $module ...");
-            $this->call('migrate',$this->getParameter($module));
+            $this->call('migrate', $this->getParameter($module));
             return $this->info("Module $module migrated.");
         }
         foreach ($this->module->all() as $name) {
@@ -29,9 +40,16 @@ class MigrateCommand extends Command
         return $this->info("All modules seeded.");
     }
 
-    protected function dbSeed($name)
+    protected function prepareDatabase()
     {
-        $this->call('migrate');
+        $this->migrator->setConnection($this->input->getOption('database'));
+
+        if ( ! $this->migrator->repositoryExists())
+        {
+            $options = array('--database' => $this->input->getOption('database'));
+
+            $this->call('migrate:install', $options);
+        }
     }
 
     protected function getArguments()
@@ -40,20 +58,18 @@ class MigrateCommand extends Command
             array('module', InputArgument::OPTIONAL, 'Name of the Module'),
         );
     }
+
     protected function getParameter($name)
     {
         $params = array();
         $params['--path'] = $this->getMigration($name);
-        if ($option = $this->option('database'))
-        {
+        if ($option = $this->option('database')) {
             $params['--database'] = $option;
         }
-        if ($option = $this->option('pretend'))
-        {
+        if ($option = $this->option('pretend')) {
             $params['--pretend'] = $option;
         }
-        if ($option = $this->option('force'))
-        {
+        if ($option = $this->option('force')) {
             $params['--force'] = $option;
         }
         return $params;
